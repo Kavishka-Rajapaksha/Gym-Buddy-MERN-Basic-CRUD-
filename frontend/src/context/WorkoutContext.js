0783b42708689872dd2da0481598b8b198f5872e
Kavish_X <143331695+Kavishka-Rajapaksha@users.noreply.github.com>
2024-11-29 22:30:24 +0530

    WorkoutsContextProvider

--- frontend/src/context/WorkoutContext.js
+++ frontend/src/context/WorkoutContext.js
@@ -1,2 +1,13 @@
 import { createContext } from "react";
 
+export const WorkoutsContext = createContext();
+
+//according to the this situation childre is app component
+const WorkoutsContextProvider = ({children})=>{
+
+    return (
+        <WorkoutsContext.Provider>
+            {children}
+        </WorkoutsContext.Provider>
+    )
+}
