c4327cb088e1e35e05449eb45d72ed7220fd927e
Kavish_X <143331695+Kavishka-Rajapaksha@users.noreply.github.com>
2024-12-01 00:37:05 +0530

    error done

--- frontend/src/context/WorkoutContext.js
+++ frontend/src/context/WorkoutContext.js
@@ -2,31 +2,29 @@ import { createContext, useReducer } from "react";
 
 export const WorkoutsContext = createContext();
 
-export const workoutReducer = (state, action)=>{
-    switch (action.type){
-        case "SET_WORKOUTS":
-            return {
-                workouts: action.payload
-            }
-        case "CREATE_WORKOUTS":
-            return {
-                workouts: [action.payload, ...state.workouts]
-            }
+export const workoutReducer = (state, action) => {
+  switch (action.type) {
+    case "SET_WORKOUTS":
+      return {
+        workouts: action.payload,
+      };
+    case "CREATE_WORKOUTS":
+      return {
+        workouts: [action.payload, ...state.workouts],
+      };
+    default:
+      return state;
+  }
+};
 
-        default:
-            return state
-    }
-}
+export const WorkoutsContextProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(workoutReducer, {
+    workouts: null,
+  });
 
-//according to the this situation childre is app component
-const WorkoutsContextProvider = ({children})=>{
-    const [state, dispatch] = useReducer(workoutReducer, {
-        workouts : null
-    })
-
-    return (
-        <WorkoutsContext.Provider value={{...state,dispatch}}>
-            {children}
-        </WorkoutsContext.Provider>
-    )
-}
+  return (
+    <WorkoutsContext.Provider value={{ ...state, dispatch }}>
+      {children}
+    </WorkoutsContext.Provider>
+  );
+};
