179a91ed72d2c58bdf4034213e64408aab786477
Kavish_X <143331695+Kavishka-Rajapaksha@users.noreply.github.com>
2024-12-04 23:04:30 +0530

    create done

--- frontend/src/components/WorkoutsDetails.js
+++ frontend/src/components/WorkoutsDetails.js
@@ -1,11 +1,19 @@
 
 const WorkoutsDetails = ({ workout }) => {
+
+    const handlerClick = async () =>{
+        const response = await fetch('/api/workouts/'+ workout._id, {
+            method: 'DELETE',
+        })
+    }
+
     return (
         <div className="workout-details">
             <h4>{workout.title}</h4>
             <p><strong>Load (kg): </strong>{workout.load}</p>
             <p><strong>Reps: </strong>{workout.reps}</p>
             <p>{workout.createdAt}</p>
+            <span onClick = {handlerClick}></span>
         </div> // Properly close the div here
     );
 };
